7e659084fb2b442f1c0df29412845e52c73a128d
“Nicholas <nicholas.pezzotti@kcl.ac.adfas>
2019-02-09 18:13:59 +0000

    Add template for login/signup page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-#@app.route("/")
+
 @app.route("/")
 def hello():
-    return render_template("index.html") 
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html") 
 
 # We only need this for local dev
 if __name__ == '__main__':

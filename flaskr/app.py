831fe3a9c12b2d360ca748b6e1fe3ea076a5bbdf
Nicholas Pezzotti <nicholas.p.pezzotti@gmail.com>
2019-02-10 17:53:18 +0000

    Create basic template for all pages to extend

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,13 +3,38 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello():
     return render_template("index.html")
 
 @app.route("/login")
 def login():
-    return render_template("login.html") 
+    return render_template("login.html")
+
+@app.route("/mentee/<k_number>")
+def mentee(k_number):
+    # ensure user is authenticated: the session is valid; the user is k_number
+
+    # user_info = getMenteeData(k_number): Dictionary
+            # Query database with k number to get the following fields:
+                # k number: string
+                # First Name: string
+                # Last Name: string
+                # Age: int
+                # Hobbies: List[string]
+                # Academic interests: List[string]
+            # Format into dictionary
+    user_info = {
+        "k_number": "k1763763",
+        "first_name": "Nicholas",
+        "last_name": "Pezzotti",
+        "age": 20,
+        "hobbies": ["football", "poker"],
+        "academic_interests": ["AI", "Blockchain"]
+    }
+
+    return render_template("user_screens/mentee_page.html", title="Your Profile", user_info=user_info)
 
 # We only need this for local dev
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5002)

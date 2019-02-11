8a1cee4e577506fe28cce6182808f8dc37779c8c
LeithanK <desiage.enzo@gmail.com>
2019-02-11 18:51:59 +0000

    Add sanity check for fields

--- flaskr/lib/database/basic.py
+++ flaskr/lib/database/basic.py
@@ -9,9 +9,13 @@ DB_HOST = "buddy-scheme.cg0eqfj7blbe.eu-west-2.rds.amazonaws.com"
 
 def query(sql_query):
     """ Returns results of query """
-
+    
+    # Connect to the database
     conn = pymysql.connect(DB_HOST, user=DATABASE_USER, password=DATABASE_PASSWORD, db=DB_NAME, connect_timeout=5)
-   
+
+    # Define result
+    result = ""   
+
     try:
     
         with conn.cursor() as cursor:
@@ -33,10 +37,13 @@ def insert(sql_query):
     # Connect to the database
     conn = pymysql.connect(DB_HOST, user=DATABASE_USER, password=DATABASE_PASSWORD, db=DB_NAME, connect_timeout=5)
 
+    # Define result
+    result = ""
+
     try:
         with conn.cursor() as cursor:
             result = cursor.execute(sql_query)
-                    
+
         conn.commit()
             
     except Exception as e:
@@ -44,10 +51,35 @@ def insert(sql_query):
 
     finally:
         conn.close()
-
+    
     return result
+
+
+def sanity_check(sql_fields):
+    """ Will sanity check the fields 
+        return true, if we can run it"""
+
+    return sql_fields.isalnum()
+
+
+def update_students(**kwargs):
+    """ Will update fields in Students based on the k_number
+        You will need to precise the specific field"""
+
+    accepted_fields = {"first_name":"", "last_name":"", "degree_title":"",
+                        "year_study":0, "gender":""}
+
+    
+    
+    # Need to check that every argument
+
+    # Need to check the type of everything    
+
+   print(k_number) 
 
 if __name__ == '__main__':
 
     print(insert("INSERT INTO Students VALUES(\"K1232323\", \"Jean\", \"Dupont\", \"Bsc Robotics\", 1, \"Other\", \"EO(*U&#H@D@#\");"))
     print(query("SELECT * FROM Students;"))
+    print(sanity_check("drop Students tables;")) 
+    print(update_students("K1631292")))

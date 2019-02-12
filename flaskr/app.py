477409ab3eb9c98de45ab4c367de79466e3d3800
Frederico A Lacs <fredlacs@gmail.com>
2019-02-12 13:47:57 +0000

    Create bruteforce login for user

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,17 +2,24 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager
 from forms import LoginForm, RegistrationForm
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
-
+db = SQLAlchemy(app)
 app.config["SECRET_KEY"]="powerful secretkey"
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+## TODO: refactor user into different file
+class User(db.Model):
+    id = db.Column(db.String(15), primary_key=True)
+    password = db.Column(db.String(15))
+    email = id + "@kcl.ac.uk"
+    first_name = db.Column(db.String(15))
+    last_name = db.Column(db.String(15))
+
 
 @app.route("/")
 @app.route("/home")
@@ -31,23 +38,40 @@ def login():
             first_name = registration_form.first_name.data
             last_name = registration_form.last_name.data
             k_number = registration_form.k_number.data
-            plain_text_password = registration_form.password.data
+            # hashed_password = generate_password_hash(registration_form.password.data)
+            hashed_password = generate_password_hash("12345678")
+
             # store user data in database
+            new_user = User(k_number=k_number, first_name=first_name, last_name=last_name, password=hashed_password)
+            
+            # db.session.add(new_user)
+            # db.session.commit()
+
+            app.logger.warning('registered')
 
             #redirect to profile page, where he must insert his preferences
             return redirect("/dashboard")
         else: # if the form was NOT valid
             # Flash the error message
+            app.logger.warning('error registering')
             return render_template("login.html", registration_form=registration_form, login_form=login_form, sign_up_visible=True)
 
     if login_form.login_submit.data: # if the login form was submitted
         if login_form.validate_on_submit(): # if the form was valid
             # check if he is authorised
-
-            #redirect to profile page, where he must insert his preferences
-            return redirect("/dashboard")
+            # get from db
+            hashed_password = generate_password_hash("12345678")
+            input_password = login_form.password.data
+            if check_password_hash(hashed_password, input_password):
+                # redirect to profile page, where he must insert his preferences
+                app.logger.warning('logged in')
+                return redirect("/dashboard")
+            else:
+                app.logger.warning('wrong password')
+                return redirect("/login")
         else: # if the form was NOT valid
             # Flash the error message
+            app.logger.warning('error logging in')
             return render_template("login.html", registration_form=registration_form, login_form=login_form)
 
     return render_template("login.html", registration_form=registration_form, login_form=login_form)

9ae1a15f1e3abe5da1c4d020ea2181437396efa1
Frederico A Lacs <fredlacs@gmail.com>
2019-02-12 13:54:28 +0000

    Update to sha256 password encryption

--- flaskr/app.py
+++ flaskr/app.py
@@ -39,7 +39,7 @@ def login():
             last_name = registration_form.last_name.data
             k_number = registration_form.k_number.data
             # hashed_password = generate_password_hash(registration_form.password.data)
-            hashed_password = generate_password_hash("12345678")
+            hashed_password = generate_password_hash("12345678", method="sha256")
 
             # store user data in database
             new_user = User(k_number=k_number, first_name=first_name, last_name=last_name, password=hashed_password)
@@ -60,7 +60,7 @@ def login():
         if login_form.validate_on_submit(): # if the form was valid
             # check if he is authorised
             ## TODO: get from db
-            hashed_password = generate_password_hash("12345678")
+            hashed_password = generate_password_hash("12345678", method="sha256")
             input_password = login_form.password.data
             if check_password_hash(hashed_password, input_password):
                 # redirect to profile page, where he must insert his preferences

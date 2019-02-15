54be5a5b7b139b6104bc281aa3b76a8d2c4008b8
Frederico A Lacs <fredlacs@gmail.com>
2019-02-15 17:14:51 +0000

    Limit user login if email confirmed in database

--- flaskr/user.py
+++ flaskr/user.py
@@ -6,9 +6,16 @@ class User(UserMixin):
         self.k_number = k_number
         self.id = k_number
         self.password = None
+        self.confirmed_email = None
 
         try:
             self.password = db.get_user_hashed_password(k_number)
+            self.confirmed_email = db.get_user_data(k_number).confirmed_email["email_confirmed"]
 
         except Exception as e:
             print("Exeception occured:{}".format(e))
+    
+    @property
+    def is_active(self):
+        # user only able to login if email is confirmed
+        return self.confirmed_email

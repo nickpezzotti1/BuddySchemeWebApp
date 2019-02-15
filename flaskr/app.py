9201c7c36b05077de94f90ceccedf5ce75513296
Effsy <jardinomidvaran@hotmail.co.uk>
2019-02-15 05:03:49 +0000

    Integrate mentor-mentee pages with db

--- flaskr/app.py
+++ flaskr/app.py
@@ -22,60 +22,60 @@ def load_user(id):
 
 # Mock data
 
-user_info = {
-    "k_number": "k1764064",
-    "k_number_mentee": "k1738383",
-    "first_name": "Nicholas",
-    "last_name": "Pezzotti",
-    "age": 20,
-    "hobbies": ["Football", "Poker"],
-    "interests": ["AI", "Blockchain"]
-}
-mentee_user_info = {
-    "k_number": "k1764064",
-    "k_number_mentee": "k1738383",
-    "first_name": "Nihad",
-    "last_name": "Rahman",
-    "age": 20,
-    "hobbies": ["Football", "Poker"],
-    "interests": ["AI", "Blockchain"]
-}
+# user_info = {
+#     "k_number": "k1764064",
+#     "k_number_mentee": "k1738383",
+#     "first_name": "Nicholas",
+#     "last_name": "Pezzotti",
+#     "age": 20,
+#     "hobbies": ["Football", "Poker"],
+#     "interests": ["AI", "Blockchain"]
+# }
+# mentee_user_info = {
+#     "k_number": "k1764064",
+#     "k_number_mentee": "k1738383",
+#     "first_name": "Nihad",
+#     "last_name": "Rahman",
+#     "age": 20,
+#     "hobbies": ["Football", "Poker"],
+#     "interests": ["AI", "Blockchain"]
+# }
 
-mentees = {
-    "k1803945" : {
-        "first_name": "Alice",
-        "last_name": "Apple",
-        "age": 21,
-    },
-    "k1874859": {
-        "first_name": "Bob",
-        "last_name": "Banana",
-        "age": 22,
-    },
-    "k1673459": {
-        "first_name": "Charlie",
-        "last_name": "Carrot",
-        "age": 19,
-    }
-}
+# mentees = {
+#     "k1803945" : {
+#         "first_name": "Alice",
+#         "last_name": "Apple",
+#         "age": 21,
+#     },
+#     "k1874859": {
+#         "first_name": "Bob",
+#         "last_name": "Banana",
+#         "age": 22,
+#     },
+#     "k1673459": {
+#         "first_name": "Charlie",
+#         "last_name": "Carrot",
+#         "age": 19,
+#     }
+# }
 
-mentors = {
-    "k1803945" : {
-        "first_name": "Jardin",
-        "last_name": "Apple",
-        "age": 21,
-    },
-    "k1874859": {
-        "first_name": "Nick",
-        "last_name": "Banana",
-        "age": 22,
-    },
-    "k1673459": {
-        "first_name": "Charlie",
-        "last_name": "Carrot",
-        "age": 19,
-    }
-}
+# mentors = {
+#     "k1803945" : {
+#         "first_name": "Jardin",
+#         "last_name": "Apple",
+#         "age": 21,
+#     },
+#     "k1874859": {
+#         "first_name": "Nick",
+#         "last_name": "Banana",
+#         "age": 22,
+#     },
+#     "k1673459": {
+#         "first_name": "Charlie",
+#         "last_name": "Carrot",
+#         "age": 19,
+#     }
+# }
 
 @app.route("/")
 @app.route("/home")
@@ -159,68 +159,85 @@ def mentor():
                 # Last Name: string
                 # Age: int
                 # Hobbies: List[string]
-                # Academic interests: List[string]
+                # Interests: List[string]
             # Format into dictionary
+    
+    print(db.get_information(current_user.k_number))
 
+    user_info = dict(db.get_information(current_user.k_number), **db.get_user_data(current_user.k_number))
     return render_template("user_screens/mentor/mentor_dashboard_page.html", title="Your Profile", user_info=user_info)
 
 @app.route("/mentee")
 def mentee():
+    user_info = dict(db.get_information(current_user.k_number), **db.get_user_data(current_user.k_number))
+    return render_template("user_screens/mentee/mentee_dashboard_page.html", title="Your Profile", user_info=user_info)
 
-    return render_template("user_screens/mentee/mentee_dashboard_page.html", title="Your Profile", user_info=mentee_user_info)
-
-@app.route("/mentor/preferences" ,methods = ['POST', 'GET'])
+@app.route("/mentor/preferences", methods = ['POST', 'GET'])
 def mentor_preferences():
 
     if request.method == "POST":
-        user_info['hobbies'] = request.form.getlist('hobbies')
-        user_info['interests'] = request.form.getlist('interests')
-        
+        # TODO: Perform validation before updating table
+        # user_info['hobbies'] = request.form.getlist('hobbies')
+        # user_info['interests'] = request.form.getlist('interests')
+        # print(request.form.getlist('hobbies'))
+        #### db.update_informations(current_user.k_number, hobbies=request.form.getlist('hobbies')[0], interests=request.form.getlist('interests')[0])
         return redirect(url_for("mentor"))
     else:
+        user_info = dict(db.get_information(current_user.k_number), **db.get_user_data(current_user.k_number))
         return render_template("user_screens/mentor/mentor_preferences_page.html", title="Your Preferences", user_info=user_info)
 
-@app.route("/mentee/preferences" ,methods = ['POST', 'GET'])
+@app.route("/mentee/preferences", methods = ['POST', 'GET'])
 def mentee_preferences():
 
     if request.method == "POST":
-        mentee_user_info['hobbies'] = request.form.getlist('hobbies')
-        mentee_user_info['interests'] = request.form.getlist('interests')
-        
+        # TODO: Perform validation before updating table
+        # mentee_user_info['hobbies'] = request.form.getlist('hobbies')
+        # mentee_user_info['interests'] = request.form.getlist('interests')
+        # print(request.form.getlist('hobbies'))
+        #### db.update_informations(current_user.k_number, hobbies=request.form.getlist('hobbies')[0], interests=request.form.getlist('interests')[0])
         return redirect(url_for("mentee"))
     else:
-        return render_template("user_screens/mentee/mentee_preferences_page.html", title="Your Preferences", user_info=mentee_user_info)
+        user_info = dict(db.get_information(current_user.k_number), **db.get_user_data(current_user.k_number))
+        return render_template("user_screens/mentee/mentee_preferences_page.html", title="Your Preferences", user_info=user_info)
 
 
 @app.route("/mentor/mentee-list")
 def mentor_mentee_list():
+    mentee_list = db.get_mentees(current_user.k_number)
 
-    # query database for all mentees the mentor has
+    # Object to hold all the mentees. This takes the form of a nested dictionary indexed by k_numbers
+    mentee_list_data = {}
 
-    return render_template("user_screens/mentor/mentor_mentee_list_page.html", title="Your Mentees", user_info=user_info, mentees=mentees)
+    # Format results into nested dict for use on page
+    for mentee in mentee_list:
+        mentee_k_number = mentee['mentee_k_number']
+        mentee_list_data[mentee_k_number] = db.get_user_data(mentee_k_number)
+
+    return render_template("user_screens/mentor/mentor_mentee_list_page.html", title="Your Mentees", mentees=mentee_list_data)
 
 @app.route("/mentee/mentor-list")
 def mentee_mentor_list():
+    mentor_list = db.get_mentors(current_user.k_number)
 
-    # query database for all mentees the mentor has
+    # Object to hold all the mentees. This takes the form of a nested dictionary indexed by k_numbers
+    mentor_list_data = {}
 
-    return render_template("user_screens/mentee/mentee_mentor_list_page.html", title="Your Mentors", user_info=mentee_user_info, mentors=mentors)
+    # Format results into nested dict for use on page
+    for mentor in mentor_list:
+        mentor_k_number = mentor['mentor_k_number']
+        mentor_list_data[mentor_k_number] = db.get_user_data(mentor_k_number)
+
+    return render_template("user_screens/mentee/mentee_mentor_list_page.html", title="Your Mentors", mentors=mentor_list_data)
 
 @app.route("/mentor/mentee/<k_number_mentee>")
 def mentor_mentee(k_number_mentee):
  
-    
-
-
-    return render_template("user_screens/mentor/mentor_mentee_page.html", title="Your Mentee", mentee_info=mentees[k_number_mentee], k_number_mentee=k_number_mentee)
+    return render_template("user_screens/mentor/mentor_mentee_page.html", title="Your Mentee", mentee_info=db.get_user_data(k_number_mentee), k_number_mentee=k_number_mentee)
 
 @app.route("/mentee/mentor/<k_number_mentor>")
 def mentee_mentor(k_number_mentor):
  
-    
-
-
-    return render_template("user_screens/mentee/mentee_mentor_page.html", title="Your Mentor", mentor_info=mentors[k_number_mentor], k_number_mentor=k_number_mentor)
+    return render_template("user_screens/mentee/mentee_mentor_page.html", title="Your Mentor", mentor_info=db.get_user_data(k_number_mentor), k_number_mentor=k_number_mentor)
 
 
 # We only need this for local dev

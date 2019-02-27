e7b841a1ee784b384fa3de84ddc6690c7d70bb96
Stoyan Binev <binevs@amazon.com>
2019-02-27 03:48:25 +0000

    Impelement singleton architecture to the DAO to ensure all models use a single data bridge

--- flaskr/models/dao.py
+++ flaskr/models/dao.py
@@ -7,82 +7,96 @@ from botocore.exceptions import ClientError
 
 
 class Dao():
-    """ Data access object for database operations"""
+    #Ssingleton class to ensure all models use only one DB bridge
 
-    SECRET = "beta/mysql"
-    REGION = "eu-west-2"
+    instance = None
+    def __init__(self, arg='Buddy'):
+        if not Dao.instance:
+            Dao.instance = Dao.__Dao(arg)
 
-    def _create_connection(self):
-        """ Estabilish database connection """
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
 
-        try:
-            self.__connection = pymysql.connect(
-                host=self._credentials['host'],
-                user = self._credentials['username'],
-                password = self._credentials['password'],
-                db = self._credentials['dbname'],
-                charset = "utf8mb4"
+    class __Dao():
+        """ Data access object for database operations"""
+
+        SECRET = "beta/mysql"
+        REGION = "eu-west-2"
+
+        def _create_connection(self):
+            """ Estabilish database connection """
+
+            try:
+                self.__connection = pymysql.connect(
+                    host=self._credentials['host'],
+                    user = self._credentials['username'],
+                    password = self._credentials['password'],
+                    db = self._credentials['dbname'],
+                    charset = "utf8mb4"
+                )
+                self.__cursor = self.__connection.cursor(pymysql.cursors.DictCursor)
+                self._log.info("Created DB connection")
+            except Exception as e:
+                self._log.exception("Could not create DB connection")
+                raise
+
+        def execute(self, query):
+            """ Execute a provided query and return result"""
+
+            try:
+                self.__cursor.execute(query)
+                return self.__cursor.fetchall()
+            except:
+                self._log.exception("Could not execute query")
+                raise
+
+        def close(self):
+            """ Close database connetion """
+
+            try:
+                self.__connection.close()
+            except:
+                self._log.exception("Could not close connection")
+                raise
+
+        def commit(self):
+            """ Commit created changes """
+
+            try:
+                self.__connection.commit()
+            except:
+                self._log.exception("Could not commit changes")
+                raise
+            
+        def _get_credentials(self):
+            """ Retreive credentials for database connection """
+
+            session = boto3.session.Session()
+            client = session.client(
+                service_name='secretsmanager',
+                region_name=self.REGION
             )
-            self.__cursor = self.__connection.cursor(pymysql.cursors.DictCursor)
-            self._log.info("Created DB connection")
-        except Exception as e:
-            self._log.exception("Could not create DB connection")
-            raise
 
-    def execute(self, query):
-        """ Execute a provided query and return result"""
-
-        try:
-            self.__cursor.execute(query)
-            return self.__cursor.fetchall()
-        except:
-            self._log.exception("Could not execute query")
-            raise
-
-    def close(self):
-        """ Close database connetion """
-
-        try:
-            self.__connection.close()
-        except:
-            self._log.exception("Could not close connection")
-            raise
-
-    def commit(self):
-        """ Commit created changes """
-
-        try:
-            self.__connection.commit()
-        except:
-            self._log.exception("Could not commit changes")
-            raise
-        
-    def _get_credentials(self):
-        """ Retreive credentials for database connection """
-
-        session = boto3.session.Session()
-        client = session.client(
-            service_name='secretsmanager',
-            region_name=self.REGION
-        )
-
-        try:
-            get_secret_value_response = client.get_secret_value(
-                SecretId=self.SECRET
-            )
-        except ClientError as e:
-            self._log.exception("Could not retrieve credentials")
-            raise e
-        else:
-            if 'SecretString' in get_secret_value_response:
-                self._credentials = json.loads(get_secret_value_response['SecretString'])
+            try:
+                get_secret_value_response = client.get_secret_value(
+                    SecretId=self.SECRET
+                )
+            except ClientError as e:
+                self._log.exception("Could not retrieve credentials")
+                raise e
             else:
-                self._credentials = json.loads(base64.b64decode(get_secret_value_response['SecretBinary']))
+                if 'SecretString' in get_secret_value_response:
+                    self._credentials = json.loads(get_secret_value_response['SecretString'])
+                else:
+                    self._credentials = json.loads(base64.b64decode(get_secret_value_response['SecretBinary']))
 
-    
-    def __init__(self,schema='Buddy'):
-        self._log = logging.getLogger(__name__)
-        self._schema = schema
-        self._get_credentials()
-        self._credentials['dbname'] = schema
-        self._create_connection()
+        def __str__(self):
+            return repr(self)
+
+        
+        def __init__(self,schema='Buddy'):
+            self._log = logging.getLogger(__name__)
+            self._schema = schema
+            self._get_credentials()
+            self._credentials['dbname'] = schema
+            self._create_connection()

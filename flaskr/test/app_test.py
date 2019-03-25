71e5afa4941c734c82dea4abef4f60ec6cf48ece
Stoyan Binev <binevs@amazon.com>
2019-03-25 13:29:21 +0000

    GET test

--- flaskr/test/app_test.py
+++ flaskr/test/app_test.py
@@ -26,21 +26,26 @@ def test_ping(client):
 
 def test_login(client):
 
-	rv = client.get('/login')
-	assert 200 == rv.status_code
+    rv = client.get('/login')
+    assert 200 == rv.status_code
 
 def test_admin(client):
 
-	rv = client.get('/admin')
-	assert 302 == rv.status_code
+    rv = client.get('/admin')
+    assert 302 == rv.status_code
 
 
 def test_mentee(client):
 
-	rv = client.get('/mentee')
-	assert 404 == rv.status_code
+    rv = client.get('/mentee')
+    assert 404 == rv.status_code
 
 def test_mentor(client):
 
-	rv = client.get('/mentor')
-	assert 404 == rv.status_code
+    rv = client.get('/mentor')
+    assert 404 == rv.status_code
+
+def test_arguments(client):
+    with flaskr.app.test_request_context('/?id=123'):
+        assert flaskr.request.path == '/'
+        assert flaskr.request.args['id'] == '123'
